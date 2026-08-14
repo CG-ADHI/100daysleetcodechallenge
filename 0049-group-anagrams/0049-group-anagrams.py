b0bc675635cc2df62fc8089825f4901ed70df743
Adithyan C G <adithyancgopi@gmail.com>
2026-08-14 22:06:05 +0530

    Time: 11 ms (82.58%), Space: 21.9 MB (79.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-         res = defaultdict(list)  # Dictionary to store groups of anagrams
+        anadict = {}
 
-         for s in strs:
-            count = [0] * 26  # Frequency array for letters a-z
+        for word in strs:
+            sortword = "".join(sorted(word))
 
-            for c in s:
-                count[ord(c) - ord('a')] += 1
+            if sortword in anadict:
+                anadict[sortword].append(word)
+            else:
+                anadict[sortword] = [word]
 
-            res[tuple(count)].append(s)
-
-         return list(res.values())
+        return list(anadict.values())
